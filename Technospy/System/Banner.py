8c2c80fb78b952a7f74bf423472ee402f13b1d6b
Veersingh Bisen <109542724+veerbisen@users.noreply.github.com>
2024-02-24 12:08:00 +0530

    Add files via upload

---
+++ Technospy/System/Banner.py
@@ -0,0 +1,25 @@
+from Colors import bcolors
+import Global
+   
+def Banner():
+	print ' _______ '
+	print '< module TECHNO-SPY >'
+	print ' ------- '
+	print '        \   '
+	print '         \                   ' 
+	print '                /\__/\ '
+	print '           ____/      \____  '   
+	print '               -x-  -x-       '
+	print '                   l           '
+	print '                ~~~w~~~             '
+	print '                   V              '
+
+	print '< GOVERNMENT POLYTECHNIC NAGPUR > '
+	print '  DEPT. OF INFORMATION TECHNOLOGY  '
+	print '       FINAL YEAR PROJECT          '
+	print '       BATCH  2021 - 2022          '
+	
+	print ' '*10 + '--=' + bcolors.OKBLUE + '[' + bcolors.ENDC + bcolors.BOLD + 'TECHNOSPY' + bcolors.ENDC
+	print ' '*7 + '--+--=' + bcolors.OKBLUE + '[' + bcolors.ENDC + 'Version : ' + bcolors.OKGREEN + bcolors.BOLD + Global.VERSION + bcolors.ENDC + bcolors.ENDC	
+	print ' '*7 + '--+--=' + bcolors.OKBLUE + '[' + bcolors.ENDC + 'Coder   : ' + bcolors.OKGREEN + bcolors.BOLD + Global.CODER + bcolors.ENDC + bcolors.ENDC	
+	print ' \n'
---
+++ Technospy/System/Global.py
@@ -0,0 +1,6 @@
+import os
+
+MAINPATH 	= os.path.abspath(os.path.join(os.path.dirname(__file__)))
+VERSION 	= '1.0.0'
+CODER		= 'veerbisen76@gmail.com'
+AGENTS 		= list()
